cc5bb5a06b33f18d641ecc0ff21b712c6010005d
Peter Abbasi <poa0@icloud.com>
2024-07-27 13:31:17 -0500

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -12,7 +12,7 @@ Else
 foreach ($csproj in $(Get-ChildItem -Recurse $PSScriptRoot -Filter *.csproj))
 {
     dotnet restore
-    dotnet publish $csproj.FullName -c Release -f net8.0 -r win7-x64 --artifacts-path $OutputDirectory -p:PublishSingleFile=true -p:PublishTrimmed=true -p:PublishReadyToRun=false
+    dotnet publish $csproj.FullName -c Release -f net8.0 -r win-x64 --artifacts-path $OutputDirectory -p:PublishSingleFile=true -p:PublishTrimmed=true -p:PublishReadyToRun=false
 }
 
 Get-ChildItem -Recurse -Path $OutputDirectory && zip -r client-win-x64.zip *
